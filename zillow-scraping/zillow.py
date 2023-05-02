63558214bd6a354d21704bf0194887e14f98df09
Andrew <ward.spacecase@gmail.com>
2023-05-01 22:11:44 -0600

    got some zillow data

--- zillow-scraping/zillow.py
+++ zillow-scraping/zillow.py
@@ -1,3 +1,27 @@
 import requests as rq
 import json
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+import regex as re
+import requests
+import lxml
+from lxml.html.soupparser import fromstring
+import prettify
+import numbers
+import htmltext
 
+request_headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.8',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
+with requests.Session() as session:
+    zip = '84401'
+    url = 'https://www.zillow.com/homes/for_sale/' + zip
+    response = session.get(url, headers=request_headers)
+    print(response.content)
+    soup = BeautifulSoup(response.content, 'html.parser')
