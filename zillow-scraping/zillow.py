e10de2edd9bc8ac1b91c6c773922d5aca4a2ce6a
Andrew <ward.spacecase@gmail.com>
2023-05-01 22:50:09 -0600

    working zillow script

--- zillow-scraping/zillow.py
+++ zillow-scraping/zillow.py
@@ -19,9 +19,40 @@ request_headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 }
 
+
 with requests.Session() as session:
     zip = '84401'
     url = 'https://www.zillow.com/homes/for_sale/' + zip
     response = session.get(url, headers=request_headers)
-    print(response.content)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'lxml')
+    # soup1 = BeautifulSoup(response.content, 'html.parser')
+    # soup2 = BeautifulSoup(response.content, 'html.parser')
+    # soup3 = BeautifulSoup(response.content, 'html.parser')
+    # soup4 = BeautifulSoup(response.content, 'html.parser')
+    df = pd.DataFrame()
+    df1 = pd.DataFrame()
+    for i in soup:
+        address = soup.find_all ('address', {'data-test':'property-card-addr'})
+        price = soup.find_all('span', {'data-test':'property-card-price'})
+        seller = soup.find_all('div', {'class':'cWiizR'})
+
+        adr=[]
+        pr=[]
+        sl=[]
+        for result in address:
+            adr.append(result.text)
+        for result in price:
+            pr.append(result.text)
+        for result in seller:
+            sl.append(result.text)
+        
+        df['seller'] = sl
+        df['address'] = adr
+        df['prices'] = pr
+        
+        df['seller'] = df['seller'].astype('str')
+        df['prices'] = df['prices'].astype('str')
+        df['address'] = df['address'].astype('str')
+
+        df = df[['prices', 'address', 'seller']]
+        print(df)
